3f40bdaf4141389f35c7e2a653a03ce49574568e
MoMeak9 <1308994506@qq.com>
2023-04-26 21:45:16 +0800

    feat: 视频转gif

--- src/videoToGif.ts
+++ src/videoToGif.ts
@@ -1,12 +1,12 @@
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
-export const video2Gif = async (videoPath: string, gifPath: string, width: number, height: number, fps: number) => {
+export const video2Gif = async (videoPath: string, gifPath: string, width: number, fps: number) => {
     ffmpeg.setFfmpegPath(ffmpegPath);
     ffmpeg(videoPath)
         .outputOptions('-pix_fmt', 'rgb24') // 设置像素格式为rgb24
-        .output('output.gif')
-        .withFPS(5) // 设置输出GIF的帧率
-        .size(`1080x?`) // 设置输出GIF的宽度，高度等比缩放
+        .output(gifPath)
+        .withFPS(fps) // 设置输出GIF的帧率
+        .size(`${width}x?`) // 设置输出GIF的宽度，高度等比缩放
         .noAudio() // 禁用音频输出
         .on('end', () => {
             console.log('转换完成！')
@@ -17,3 +17,6 @@ export const video2Gif = async (videoPath: string, gifPath: string, width: numbe
 
 video2Gif('F:\\个人项目\\img-service\\src\\1.mp4', 'F:\\个人项目\\img-service\\src\\1.gif', 300, 300, 10);
 
+// 当前执行目录
+// 匹配是否为绝对路径
+// 下载网络图片
